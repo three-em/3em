a1bffb21e14034598b4eb693423b3e569f250127
Divy Srivastava <dj.srivastava23@gmail.com>
2021-11-17 12:16:32 +0530

    test

---
+++ src/runtime/testdata/01_wasm.rs
@@ -0,0 +1,34 @@
+use serde::Deserialize;
+use serde::Serialize;
+
+#[derive(Serialize, Deserialize)]
+pub struct State {
+  counter: i32,
+}
+
+#[derive(Deserialize)]
+pub struct Action {}
+
+fn neat_handle(state: State, _action: Action) -> State {
+  State { counter: state.counter + 1 }
+}
+
+#[no_mangle]
+pub extern fn handle(
+  state: *const u8,
+  state_size: usize,
+  action: *const u8,
+  action_size: usize,
+  output: *mut *const u8,
+  output_size: *mut usize,
+) {
+  let state_buf = unsafe { slice::from_raw_parts(state, state_size) };
+  let state: State = serde_json::from_slice(state_buf).unwrap();
+  let action_buf = unsafe { slice::from_raw_parts(action, action_size) };
+  let action: Action = serde_json::from_slice(action_buf).unwrap();
+
+  let output_state = neat_handle(state, action);
+  let output_buf = serde_json::to_vec(&output_state).unwrap();
+  *output = output_buf.as_slice().as_ptr();
+  *output_size = output_buf.len();
+}
