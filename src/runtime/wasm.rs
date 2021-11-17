e9654886debaadbf8e5b15e1fba6556077abe7a2
Divy Srivastava <dj.srivastava23@gmail.com>
2021-11-17 13:54:46 +0530

    more test

--- src/runtime/wasm.rs
+++ src/runtime/wasm.rs
@@ -1,8 +1,12 @@
 use deno_core::error::AnyError;
 use deno_core::JsRuntime;
+use serde::de::DeserializeOwned;
+use serde::Serialize;
+use std::fmt::Debug;
 
 pub struct WasmRuntime {
   rt: JsRuntime,
+  handle: v8::Global<v8::Value>,
 }
 
 impl WasmRuntime {
@@ -32,13 +36,68 @@ impl WasmRuntime {
       global.set(scope, name.into(), v8_module);
     }
 
-    Ok(Self { rt })
+    {
+      let v8_instance = rt
+        .execute_script("<anon>", "new WebAssembly.Instance(WASM_MODULE)")
+        .unwrap();
+
+      let scope = &mut rt.handle_scope();
+
+      let global = global.open(scope).global(scope);
+      let v8_instance = v8::Local::new(scope, v8_instance);
+
+      let name = v8::String::new(scope, "WASM_INSTANCE").unwrap();
+      global.set(scope, name.into(), v8_instance);
+
+      let name = v8::String::new(scope, "EXPORT_NAME").unwrap();
+      let func_name = v8::String::new(scope, "handle").unwrap();
+      global.set(scope, name.into(), func_name.into());
+    }
+
+    let handle = rt
+      .execute_script("<anon>", "WASM_INSTANCE.exports[EXPORT_NAME]")
+      .unwrap();
+
+    Ok(Self { rt, handle })
+  }
+
+  pub async fn call<R, T>(&mut self, arguments: &[R]) -> Result<T, AnyError>
+  where
+    R: Serialize + 'static,
+    T: Debug + DeserializeOwned + 'static,
+  {
+    let global = {
+      let scope = &mut self.rt.handle_scope();
+      let arguments: Vec<v8::Local<v8::Value>> = arguments
+        .iter()
+        .map(|argument| deno_core::serde_v8::to_v8(scope, argument).unwrap())
+        .collect();
+      let module_obj = self.handle.get(scope).to_object(scope).unwrap();
+      let func = v8::Local::<v8::Function>::try_from(module_obj)?;
+
+      let undefined = v8::undefined(scope);
+      let local = func.call(scope, undefined.into(), &arguments).unwrap();
+      v8::Global::new(scope, local)
+    };
+
+    let result: T = {
+      // Run the event loop.
+      let value = self.rt.resolve_value(global).await?;
+      let scope = &mut self.rt.handle_scope();
+
+      let value = v8::Local::new(scope, value);
+      deno_core::serde_v8::from_v8(scope, value)?
+    };
+
+    Ok(result)
   }
 }
 
 #[cfg(test)]
 mod tests {
   use crate::runtime::wasm;
+  use deno_core::serde_json::json;
+  use deno_core::serde_json::Value;
 
   #[tokio::test]
   async fn test_wasm_runtime() {
@@ -47,7 +106,16 @@ mod tests {
 
   #[tokio::test]
   async fn test_wasm_runtime_contract() {
-    let _rt =
-      wasm::WasmRuntime::new(include_bytes!("./testdata/01_wasm/01_wasm.wasm"));
+    let mut rt =
+      wasm::WasmRuntime::new(include_bytes!("./testdata/01_wasm/01_wasm.wasm"))
+        .await
+        .unwrap();
+
+    let state: Value = rt
+      .call(&[json!({
+        "counter": 0,
+      })])
+      .await
+      .unwrap();
   }
 }
