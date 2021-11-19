2f8fac9a30c1584f1852a86619db1c4458776224
andreespirela <andreespirela@outlook.com>
2021-11-18 20:45:20 -0500

    getAddr

--- src/messages/get_addr.rs
+++ src/messages/get_addr.rs
@@ -2,7 +2,8 @@ use crate::node::Node;
 
 pub fn get_addr(my_node: &Node) -> String {
   format!(
-    "host:{}\nversion:{}",
+    "command:{}\nhost:{}\nversion:{}",
+    "getAddr",
     my_node.to_string(),
     env!("CARGO_PKG_VERSION")
   )
