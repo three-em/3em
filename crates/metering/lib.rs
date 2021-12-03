75ef124c3659ef7b42f050319fb8c589adc8bbb8
Divy Srivastava <dj.srivastava23@gmail.com>
2021-12-03 19:46:30 +0530

    preserve authors

--- crates/metering/lib.rs
+++ crates/metering/lib.rs
@@ -971,7 +971,10 @@ mod tests {
     let metering = Metering::new(test_cost_function);
     // (expected gas consumption, module bytes)
     let sources: [(usize, &[u8]); 5] = [
-      (9972855, include_bytes!("../../testdata/01_wasm/01_wasm.wasm")),
+      (
+        9990246,
+        include_bytes!("../../testdata/01_wasm/01_wasm.wasm"),
+      ),
       (
         3263,
         include_bytes!("../../helpers/rust/example/contract.wasm"),
