5329b0ec3e9849d73f7f2996a813a5a264ca4579
andreespirela <andreespirela@outlook.com>
2021-12-07 14:10:05 -0500

    Fixing CLI

--- crates/cli/cli/parse.rs
+++ crates/cli/cli/parse.rs
@@ -60,7 +60,13 @@ pub fn parse() -> Result<Flags, pico_args::Error> {
       save_path: pargs
         .opt_value_from_str("--save")?
         .unwrap_or(String::from("")),
-      height: pargs.opt_value_from_str("--height")?,
+      height: {
+        if let Some(data) = pargs.opt_value_from_str("--height").unwrap() {
+          Some(data)
+        } else {
+          None
+        }
+      },
       no_cache: pargs.contains("--no-cache"),
     },
   };
