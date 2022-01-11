6b8773f2ec8d43c40b6daae3325d846ae472559e
andreespirela <andreespirela@outlook.com>
2022-01-11 12:04:52 -0500

    Adding help and handling panics better

--- crates/cli/cli/parse.rs
+++ crates/cli/cli/parse.rs
@@ -1,4 +1,6 @@
+use crate::print_help::print_help;
 use pico_args::Arguments;
+use std::ops::Deref;
 
 #[derive(Debug)]
 pub enum Flags {
@@ -11,7 +13,7 @@ pub enum Flags {
     host: String,
     port: i32,
     protocol: String,
-    tx: String,
+    tx: Option<String>,
     pretty_print: bool,
     no_print: bool,
     show_validity: bool,
@@ -28,8 +30,14 @@ pub enum Flags {
     protocol: String,
     pretty_print: bool,
     show_validity: bool,
-    file: String,
+    file: Option<String>,
   },
+}
+
+#[derive(Debug)]
+pub enum ParseResult {
+  Help { cmd: String },
+  Known { flag: Flags },
 }
 
 fn parse_node_limit(
@@ -42,58 +50,74 @@ fn parse_node_limit(
   Ok(node_limit)
 }
 
-pub fn parse() -> Result<Flags, pico_args::Error> {
+pub fn parse() -> Result<ParseResult, pico_args::Error> {
   let mut pargs = Arguments::from_env();
+  let is_help = pargs.contains("--help");
 
-  #[allow(clippy::wildcard_in_or_patterns)]
-  let flags = match pargs.subcommand()?.as_deref().unwrap_or("Unknown") {
-    "start" => Flags::Start {
-      port: pargs.opt_value_from_str("--port")?.unwrap_or(8755),
-      host: pargs
-        .opt_value_from_str("--host")?
-        .unwrap_or_else(|| String::from("127.0.0.1")),
-      node_capacity: parse_node_limit(&mut pargs).unwrap(),
-    },
-    "dry-run" => Flags::DryRun {
-      host: pargs
-        .opt_value_from_str("--arweave-host")?
-        .unwrap_or_else(|| String::from("arweave.net")),
-      port: pargs.opt_value_from_str("--arweave-port")?.unwrap_or(80),
-      protocol: pargs
-        .opt_value_from_str("--arweave-protocol")?
-        .unwrap_or_else(|| String::from("https")),
-      pretty_print: pargs.contains("--pretty-print"),
-      show_validity: pargs.contains("--show-validity"),
-      file: pargs
-        .value_from_str("--file")
-        .expect("dry-run requires --file"),
-    },
-    "run" | _ => Flags::Run {
-      host: pargs
-        .opt_value_from_str("--arweave-host")?
-        .unwrap_or_else(|| String::from("arweave.net")),
-      port: pargs.opt_value_from_str("--arweave-port")?.unwrap_or(80),
-      protocol: pargs
-        .opt_value_from_str("--arweave-protocol")?
-        .unwrap_or_else(|| String::from("https")),
-      tx: pargs
-        .opt_value_from_str("--contract-id")?
-        .unwrap_or_else(|| {
-          String::from("t9T7DIOGxx4VWXoCEeYYarFYeERTpWIC1V3y-BPZgKE")
-        }),
-      pretty_print: pargs.contains("--pretty-print"),
-      no_print: pargs.contains("--no-print"),
-      show_validity: pargs.contains("--show-validity"),
-      save: pargs.contains("--save"),
-      benchmark: pargs.contains("--benchmark"),
-      save_path: pargs
-        .opt_value_from_str("--save")?
-        .unwrap_or_else(|| String::from("")),
-      height: { pargs.opt_value_from_str("--height").unwrap() },
-      no_cache: pargs.contains("--no-cache"),
-      show_errors: pargs.contains("--show-errors"),
-    },
-  };
+  let cmd = pargs
+    .subcommand()?
+    .as_deref()
+    .unwrap_or("Unknown")
+    .to_string();
 
-  Ok(flags)
+  if is_help {
+    Ok(ParseResult::Help {
+      cmd: String::from(cmd),
+    })
+  } else {
+    #[allow(clippy::wildcard_in_or_patterns)]
+    let flags = match cmd.deref() {
+      "start" => ParseResult::Known {
+        flag: Flags::Start {
+          port: pargs.opt_value_from_str("--port")?.unwrap_or(8755),
+          host: pargs
+            .opt_value_from_str("--host")?
+            .unwrap_or_else(|| String::from("127.0.0.1")),
+          node_capacity: parse_node_limit(&mut pargs).unwrap(),
+        },
+      },
+      "dry-run" => ParseResult::Known {
+        flag: Flags::DryRun {
+          host: pargs
+            .opt_value_from_str("--arweave-host")?
+            .unwrap_or_else(|| String::from("arweave.net")),
+          port: pargs.opt_value_from_str("--arweave-port")?.unwrap_or(80),
+          protocol: pargs
+            .opt_value_from_str("--arweave-protocol")?
+            .unwrap_or_else(|| String::from("https")),
+          pretty_print: pargs.contains("--pretty-print"),
+          show_validity: pargs.contains("--show-validity"),
+          file: pargs.opt_value_from_str("--file").unwrap(),
+        },
+      },
+      "run" => ParseResult::Known {
+        flag: Flags::Run {
+          host: pargs
+            .opt_value_from_str("--arweave-host")?
+            .unwrap_or_else(|| String::from("arweave.net")),
+          port: pargs.opt_value_from_str("--arweave-port")?.unwrap_or(80),
+          protocol: pargs
+            .opt_value_from_str("--arweave-protocol")?
+            .unwrap_or_else(|| String::from("https")),
+          tx: pargs.opt_value_from_str("--contract-id").unwrap(),
+          pretty_print: pargs.contains("--pretty-print"),
+          no_print: pargs.contains("--no-print"),
+          show_validity: pargs.contains("--show-validity"),
+          save: pargs.contains("--save"),
+          benchmark: pargs.contains("--benchmark"),
+          save_path: pargs
+            .opt_value_from_str("--save")?
+            .unwrap_or_else(|| String::from("")),
+          height: { pargs.opt_value_from_str("--height").unwrap() },
+          no_cache: pargs.contains("--no-cache"),
+          show_errors: pargs.contains("--show-errors"),
+        },
+      },
+      "Unknown" | _ => ParseResult::Help {
+        cmd: String::from("none"),
+      },
+    };
+
+    Ok(flags)
+  }
 }
---
+++ crates/cli/print_help.rs
@@ -0,0 +1,47 @@
+use indoc::indoc;
+
+pub fn print_help(sub_command: Option<&str>) {
+  let help = match sub_command.unwrap_or("none") {
+    "dry-run" => indoc! {"
+            three_em dry-run [options]
+
+            Runs a local contract with local interactions provided in a configuration file.
+
+            Options:
+                --arweave-host   Gateway url to be used by SmartWeave APIs   (Default: arweave.net)   [string]
+                --arweave-port   Gateway port to be used   (Default: 443)   [string]
+                --arweave-protocol   Protocol to be used for gateway communication (Default: https)   [http|https]
+                --pretty-print   Whether state result should be in JSON prettified form   (Default: false)   [boolean]
+                --show-validity   Whether validity table should be included in output   (Default: false)   [boolean]
+                --file   Path to configuration file to be used   (Required)   [string]
+    "},
+    "run" => indoc! {"
+            three_em run [options]
+
+            Runs a contract deployed to the Arweave network.
+
+            Options:
+                --contract-id  ID of contract to be evaluated   (Required)   [string]
+                --arweave-host   Gateway url to be used by Executor & SmartWeave APIs   (Default: arweave.net)   [string]
+                --arweave-port   Gateway port to be used   (Default: 443)   [string]
+                --arweave-protocol   Protocol to be used for gateway communication (Default: https)   [http|https]
+                --pretty-print   Whether state result should be in JSON prettified form   (Default: false)   [boolean]
+                --show-validity   Whether validity table should be included in output   (Default: false)   [boolean]
+                --no-print   Whether no output should be displayed   (Default: false)   [boolean]
+                --benchmark   Whether execution time should be displayed   (Default: false)   [boolean]
+                --no-cache   Whether cache system should be used for evaluation   (Default: true)   [boolean]
+                --show-errors   Whether exceptions thrown during evaluation should be shown   (Default: false)   [boolean]
+                --save   Path to file where output will be saved   [string]
+                --height   Maximum height to be evaluated   [number]
+    "},
+    "none" | _ => indoc! {"
+            three_em <command> [options]
+
+            Commands:
+                three_em run [options]   Evaluates the latest state of a deployed contract.
+                three_em dry-run [options]   Evaluates the latest state of a local contract.
+    "},
+  };
+
+  println!("{}", help);
+}
