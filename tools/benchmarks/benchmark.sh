3858559328f768e09af31574c57f018f5440271d
Maximous Black <maximousblk@gmail.com>
2022-02-11 09:55:16 +0000

    bench: no ignore failure

--- tools/benchmarks/benchmark.sh
+++ tools/benchmarks/benchmark.sh
@@ -1,12 +1,11 @@
 hyperfine \
-  '../../target/release/bench' \
-  '../../target/release/bench_evm' \
   '../../target/release/bench_wasm' \
+  '../../target/release/bench_evm' \
   '../../target/release/bench_fh' \
+  '../../target/release/bench' \
   'node ./smartweave/index.js' \
   --runs 20 \
   --warmup 5 \
-  --ignore-failure \
   --time-unit "millisecond" \
   --export-json "./results.json" \
   --export-markdown "./results.md"
