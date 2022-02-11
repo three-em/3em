eee1aaeec1ed3d972ae64da04e103592c09e31a1
Maximous Black <maximousblk@gmail.com>
2022-02-11 17:58:14 +0000

    add graphs to benchmark comments

---
+++ tools/benchmarks/mdimages.sh
@@ -0,0 +1,12 @@
+imgupload() {
+  curl 'https://ipfs.infura.io:5001/api/v0/add?pin=true&cid-version=1' \
+    -F "path=@$1" \
+    --compressed --silent |
+    jq -r '.Hash'
+}
+
+echo "\n<details><summary>Graphs</summary>\n"
+for file in "$@"; do
+  echo "![$(basename $file)](https://ipfs.infura.io/ipfs/$(imgupload $file))"
+done
+echo "\n</details>\n"
