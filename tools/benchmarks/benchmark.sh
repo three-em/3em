b817495246eccfeafa723ac24141a7c8b48f00fe
Maximous Black <maximousblk@gmail.com>
2022-02-11 11:47:51 +0000

    fix bench script

--- tools/benchmarks/benchmark.sh
+++ tools/benchmarks/benchmark.sh
@@ -2,10 +2,10 @@ REPO=$(git rev-parse --show-toplevel)
 BUILDS=${BUILDS:-$REPO/target/release}
 
 hyperfine \
-  "$BUILDS/bench_wasm" \
-  "$BUILDS/bench_evm" \
-  "$BUILDS/bench_fh" \
-  "$BUILDS/bench" \
+  "$REPO/$BUILDS/bench_wasm" \
+  "$REPO/$BUILDS/bench_evm" \
+  "$REPO/$BUILDS/bench_fh" \
+  "$REPO/$BUILDS/bench" \
   "node ./smartweave/index.js" \
   --runs 20 \
   --warmup 5 \
